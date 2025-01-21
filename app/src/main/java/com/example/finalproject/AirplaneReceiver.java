686646c71d8415625ac38d0040245793e74fbd6b
Deep Raj <deepraj6897@gmail.com>
2025-01-21 10:11:53 +0530

    Final travel app

---
+++ app/src/main/java/com/example/finalproject/AirplaneReceiver.java
@@ -0,0 +1,24 @@
+package com.example.finalproject;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class AirplaneReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        String intentAction = intent.getAction();
+        if (intentAction != null){
+            String toastmessage = "unknown intent action";
+            if (intentAction.equals(Intent.ACTION_AIRPLANE_MODE_CHANGED)) {
+                toastmessage = "Airplane mode changed";
+            }
+            //display the toast
+            Toast.makeText(context, toastmessage,Toast.LENGTH_SHORT).show();
+        }
+    }
+}
